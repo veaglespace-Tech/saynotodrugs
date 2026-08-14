3f7c1d915a4966c24665c1d204e14e5d99be7a06
Poteritesh <riteshpote0603@gmail.com>
2026-08-14 19:31:47 +0530

    Optimize Next.js build config to prevent OOM errors

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  productionBrowserSourceMaps: false,
+  output: 'standalone',
 };
 
 export default nextConfig;
