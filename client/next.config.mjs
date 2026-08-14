0769884e64fd4ede7456bbc4fa56f6e6978f6477
Poteritesh <riteshpote0603@gmail.com>
2026-08-14 19:52:49 +0530

    Fix CI deployment freeze by limiting NODE_OPTIONS memory and fixing next.config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   productionBrowserSourceMaps: false,
   output: 'standalone',
 };
